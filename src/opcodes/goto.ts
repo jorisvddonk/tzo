378029bc48272a867cd5f1934552053adfe7dcaa
joris <jorisvddonk@gmail.com>
2020-09-22 23:30:24 +0200

    goto: support jump to pc

--- src/opcodes/goto.ts
+++ src/opcodes/goto.ts
@@ -1,12 +1,17 @@
 import { Stack, Context, getStackParams, VM } from "..";
 
 export default function goto(stack: Stack, context: Context, vm: VM) {
-  const [str1] = getStackParams("goto", ["string"], stack) as [string];
-  const newPC = vm.labelMap[str1];
-  if (newPC === undefined) {
-    throw new Error(`goto: attempting to jump to undefined label ${str1}`);
+  const [val1] = getStackParams("goto", ["string | number"], stack) as [string | number];
+  if (typeof val1 === "string") {
+    const newPC = vm.labelMap[val1];
+    if (newPC === undefined) {
+      throw new Error(`goto: attempting to jump to undefined label ${val1}`);
+    }
+    vm.logger(`goto: setting pC to ${newPC} (goto ${val1}) `);
+    vm.programCounter = newPC;
+    return null;
+  } else if (typeof val1 === "number") {
+    vm.logger(`goto: setting pC to ${val1} (goto ${val1}) `);
+    vm.programCounter = val1;
   }
-  vm.logger(`goto: setting pC to ${newPC} `);
-  vm.programCounter = newPC;
-  return null;
 }
